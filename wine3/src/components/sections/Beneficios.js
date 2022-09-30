374457d9850c7476c937721efc690c105f9e7188
Gera Barboni <gerardobarboni@gmail.com>
2022-09-30 10:00:27 -0300

    beneficios v2

--- wine3/src/components/sections/Beneficios.js
+++ wine3/src/components/sections/Beneficios.js
@@ -5,16 +5,51 @@ const Beneficios = () => {
   return (
     <div className="bg-white h-screen flex flex-col font-poppins">
       <div className="h-[6.5%] flex w-full shadow">
-        <img></img>
         <p className="text-[24px] font-medium ml-4 my-auto">
           Beneficios exclusivos
         </p>
       </div>
-      <div className="w-11/12 h-[93.5%] flex flex-col justify-around m-auto">
-        <div className="flex">
-          <p>
+      <div className="h-[93.5%] flex flex-col justify-between m-auto">
+        <div className="flex justify-between">
+          <img src={cup} alt="cup" className="h-[200px]"></img>
+          <p className="text-12 font-light italic text-center m-auto">
             Viví la experiencia, degusta la cosecha exclusiva de esta bodega.{' '}
           </p>
+        </div>
+        <div className="flex flex-col h-full justify-evenly">
+          <div className="bg-lightGrey rounded-[19px] text-darkGrey p-5">
+            <div className="flex">
+              <p className="mr-2 font-light text-[24px]">Beneficio I </p>
+              <p className="font-medium text-[24px]">| White List</p>
+            </div>
+            <p className="text-[12px]">
+              Formar parte de una lista por la cual tendrás acceso a
+              producciones a modo de preventa, además de acceder a cosechas
+              exclusivas.
+            </p>
+          </div>
+          <div className="bg-lightGrey rounded-[19px] text-darkGrey p-5">
+            <div className="flex">
+              <p className="mr-2 font-light text-[24px]">Beneficio II </p>
+              <p className="font-medium text-[24px]"> | Visitas</p>
+            </div>
+            <p className="text-[12px]">
+              Tendrás acceso a realizar una visita a las instalaciones de la
+              bodega, y conocer a cerca de la creación del producto de forma
+              física.
+            </p>
+          </div>
+          <div className="bg-lightGrey rounded-[19px] text-darkGrey p-5">
+            <div className="flex">
+              <p className="mr-2 font-light text-[24px]">Beneficio III </p>
+              <p className="font-medium text-[24px]"> | Catas</p>
+            </div>
+            <p className="text-[12px]">
+              Podrás disfrutar y deleitarte con la cata de los más exclusivos
+              vinos de Elefante, mientras escuchas datos de valor a cerca de su
+              composición.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- wine3/src/components/sections/ComoFunciona.js
+++ wine3/src/components/sections/ComoFunciona.js
@@ -6,12 +6,12 @@ const ComoFunciona = () => {
       <div className="h-[6.5%] flex w-full shadow">
         <p className="text-[24px] font-medium ml-4 my-auto">¿Como funciona?</p>
       </div>
-      <div className="w-11/12 h-[93.5%] flex flex-col justify-around m-auto">
-        <p className="text-[16px] p-5 bg-white rounded-[20px] font-light">
+      <div className="w-11/12 h-[93.5%] flex flex-col justify-around m-auto ">
+        <p className="text-[16px] p-5 rounded-[20px] font-light bg-lightGrey text-darkGrey">
           Conectamos Bodegas locales y su producto con el mundo, a través del
           uso de la tecnología blockchain
         </p>
-        <p className="text-[16px] p-5 bg-white rounded-[20px] font-light">
+        <p className="text-[16px] p-5 rounded-[20px] font-light bg-lightGrey text-darkGrey">
           Ofrecémos un producto digital bien conocido como NFT con múltiples
           beneficios que lo respaldan y le brindan gran valor, a los que podrá
           acceder efectuando la compra del mismo por medio los medios que
