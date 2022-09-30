01968d9e036f24d9d2ed43cf1d5d6743d1004e39
Gera Barboni <gerardobarboni@gmail.com>
2022-09-30 09:31:43 -0300

    como funciona + beneficios v1

--- wine3/src/components/sections/Beneficios.js
+++ wine3/src/components/sections/Beneficios.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import cup from '../../assets/cup.png';
 
 const Beneficios = () => {
   return (
-    <div className="bg-white h-screen flex font-poppins">
-      <div className="w-11/12 h-full m-auto pt-24">
-        <p>Beneficios</p>
+    <div className="bg-white h-screen flex flex-col font-poppins">
+      <div className="h-[6.5%] flex w-full shadow">
+        <img></img>
+        <p className="text-[24px] font-medium ml-4 my-auto">
+          Beneficios exclusivos
+        </p>
+      </div>
+      <div className="w-11/12 h-[93.5%] flex flex-col justify-around m-auto">
+        <div className="flex">
+          <p>
+            Viví la experiencia, degusta la cosecha exclusiva de esta bodega.{' '}
+          </p>
+        </div>
       </div>
     </div>
   );
--- wine3/src/components/sections/ComoFunciona.js
+++ wine3/src/components/sections/ComoFunciona.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const ComoFunciona = () => {
   return (
-    <div className="bg-white h-screen flex font-poppins">
-      <div className="w-11/12 h-full m-auto pt-24">
-        <p className="text-[48px] font-black">Como funciona</p>
+    <div className="bg-white h-screen flex flex-col font-poppins">
+      <div className="h-[6.5%] flex w-full shadow">
+        <p className="text-[24px] font-medium ml-4 my-auto">¿Como funciona?</p>
+      </div>
+      <div className="w-11/12 h-[93.5%] flex flex-col justify-around m-auto">
+        <p className="text-[16px] p-5 bg-white rounded-[20px] font-light">
+          Conectamos Bodegas locales y su producto con el mundo, a través del
+          uso de la tecnología blockchain
+        </p>
+        <p className="text-[16px] p-5 bg-white rounded-[20px] font-light">
+          Ofrecémos un producto digital bien conocido como NFT con múltiples
+          beneficios que lo respaldan y le brindan gran valor, a los que podrá
+          acceder efectuando la compra del mismo por medio los medios que
+          pusimos a su disposición.
+        </p>
       </div>
     </div>
   );
