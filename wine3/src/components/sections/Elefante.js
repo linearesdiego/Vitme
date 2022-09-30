c106acfd9744e217dad5bee9a73555764d22a1cb
nachorosa <nachhitorosa@icloud.com>
2022-09-30 15:19:38 -0300

    equipo, elefante y contacto estructura

--- wine3/src/components/sections/Elefante.js
+++ wine3/src/components/sections/Elefante.js
@@ -3,9 +3,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import 'swiper/css/pagination';
 import 'swiper/css';
+// import fondo from '../../assets/elefante/background';
+import historia from '../../assets/elefante/historia.png';
+import sobrefeliyjuli from '../../assets/elefante/sobrefeliyjuli.png';
+import tripleimpacto from '../../assets/elefante/tripleimpacto.png';
+
+
 
 const Elefante = () => {
   return (
+
+
+ 
     <Swiper
       className="mySwiper2 h-screen"
       spaceBetween={0}
@@ -14,9 +23,75 @@ const Elefante = () => {
       }}
       modules={[Pagination]}
     >
-      <SwiperSlide className="pt-24">Elefante 1</SwiperSlide>
-      <SwiperSlide className="pt-24">Elefante 2</SwiperSlide>
-      <SwiperSlide className="pt-24">Elefante 3</SwiperSlide>
+
+      <SwiperSlide className="h-full">
+              <div className="h-14 flex w-full shadow ">
+                <p className="text-[24px] font-medium ml-4 my-auto">Elefante Wine</p>
+              </div>
+              <div className="flex flex-col justify-evenly h-full bg-orange-600">
+                <div>
+                  <p className="text-[16px]">
+                  Nace de la unión de una pareja de enamorados y su pasión por los vinos, Felipe y Juliana, son autores de Elefante Wine, una bodega en la que la familia produce sus vinos de forma cien por ciento casera, en el garaje de su hogar.
+                  Elefante pone a nuestro alcance productos que han logrado gran prestigio y renombre por la calidad del producto.
+                  </p>
+                </div>
+                <div>
+                  <a href="#" className="rounded-[14px] font-semibold text-[20px] py-[10px] px-[60px] bg-black text-white mr-auto" >
+                    Conocer más
+                  </a>
+                </div>
+              </div>
+      </SwiperSlide>
+      <SwiperSlide className="h-full">
+              <div className="h-14 flex w-full shadow ">
+                <p className="text-[24px] font-medium ml-4 my-auto">Historia del nombre</p>
+              </div>
+              <div className="flex flex-col justify-evenly h-full bg-orange-600">
+                <div>
+                  <p className="text-[16px]">
+                  Los Elefantes tienen una prodigiosa memoria asociada al olfato. Los humanos tenemos una memoria olfativa asociada a nuestras emociones. Un vino que no pretende ser memorable, sino que intenta tocar una fibra, algo que nos haga recordar.
+                  </p>
+                </div>
+                <div>
+                <img src={historia} alt="historia" className='rounded-xl' ></img>
+                </div>
+              </div>
+      </SwiperSlide>
+      <SwiperSlide className="h-full">
+              <div className="h-14 flex w-full shadow ">
+                <p className="text-[24px] font-medium ml-4 my-auto">Sobre Feli y Juli</p>
+              </div>
+              <div className="flex flex-col justify-evenly h-full bg-orange-600">
+                <div>
+                  <p className="text-[16px]">
+                  Su historia comenzó en la Facultad de Ciencias Agrarias de UNCuyo, donde además del amor, nació el proyecto enológico. El matrimonio comparte la misma pasión por la vitivinicultura, por lo que a la hora emprender se complementaron y formaron un buen equipo de trabajo.
+                  </p>
+                </div>
+                <div>
+                <img src={sobrefeliyjuli} alt="sobrefeliyjuli" className='rounded-xl' ></img>
+                </div>
+              </div>
+      </SwiperSlide>
+      <SwiperSlide className="h-full">
+              <div className="h-14 flex w-full shadow ">
+                <p className="text-[24px] font-medium ml-4 my-auto">Triple Impacto</p>
+              </div>
+              <div className="flex flex-col justify-evenly h-full bg-orange-600">
+                <div>
+                  <p className="text-[16px]">
+                    Está ampliamente comprometido con el 
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[16px]">
+                    Lörem ipsum pokul sena av krovöse sas. Plasamma ubel i kvasin. Von täns.
+                  </p>
+                </div>
+                <div>
+                <img src={tripleimpacto} alt="tripleimpacto" className='rounded-xl' ></img>
+                </div>
+              </div>
+      </SwiperSlide>
     </Swiper>
   );
 };
--- wine3/src/components/sections/Equipo.js
+++ wine3/src/components/sections/Equipo.js
@@ -3,6 +3,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import 'swiper/css/pagination';
 import 'swiper/css';
+import federico from '../../assets/equipo/federico.png';
+import rodrigo from '../../assets/equipo/rodrigo.png';
+import kevin from '../../assets/equipo/kevin.png';
+import linkedin from '../../assets/equipo/linkedin.png';
+import twitter from '../../assets/equipo/twitter.png';
+
+
 
 const Equipo = () => {
   return (
@@ -22,8 +29,146 @@ const Equipo = () => {
             clickable: true,
           }}
         >
-          <SwiperSlide className="pt-24">Equipo 1</SwiperSlide>
-          <SwiperSlide className="pt-24">Equipo 2</SwiperSlide>
+          <SwiperSlide className="h-full">
+                  <div className="h-14 flex w-full shadow ">
+                    <p className="text-[24px] font-medium ml-4 my-auto">El equipo</p>
+                  </div>
+                  <div className="flex flex-col justify-evenly">
+                    <div>
+                      <p className="text-[16px]">
+                      Buscamos conectar a las bodegas locales con el mundo entero, a través del uso de la tecnología blockchain. Para lograr este cometido hemos reunido el siguiente equipo de profesionales
+                      </p>
+                    </div>
+                    <div className='h-full'>
+
+                    <div className='bg-gray-200 m-5 rounded-lg flex items-center'>
+                        <div>
+                          <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        </div>
+                        <div>
+                          <h2>Federico Marino</h2>
+                          <p>Co Founder</p>
+                          <div className='flex flex-row'>
+                            <div className='bg-gray-400'>
+                              <img src={linkedin} alt="linkedin" />
+                            </div>
+                            <div className='bg-gray-400'>
+                              <img src={twitter} alt="twitter" />
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+
+                    <div className='bg-gray-200 m-5 rounded-lg flex items-center'>
+                        <div>
+                          <img src={rodrigo} alt="rodrigo" className='rounded-xlinline-block rounded-full' />
+                        </div>
+                        <div>
+                          <h2>Rodrigo Dománico</h2>
+                          <p>Co Founder</p>
+                          <div className='flex flex-row'>
+                            <div className='bg-gray-400'>
+                              <img src={linkedin} alt="linkedin" />
+                            </div>
+                            <div className='bg-gray-400'>
+                              <img src={twitter} alt="twitter" />
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+
+                    <div className='bg-gray-200 m-5 rounded-lg flex items-center'>
+                        <div>
+                          <img src={kevin} alt="kevin" className='rounded-xlinline-block rounded-full' />
+                        </div>
+                        <div>
+                          <h2>Kevin Willenberg</h2>
+                          <p>Co Founder</p>
+                          <div className='flex flex-row'>
+                            <div className='bg-gray-400'>
+                              <img src={linkedin} alt="linkedin" />
+                            </div>
+                            <div className='bg-gray-400'>
+                              <img src={twitter} alt="twitter" />
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+
+                    </div>
+
+                  </div>
+          </SwiperSlide>
+          <SwiperSlide className="h-full">
+                  <div className="h-14 flex w-full shadow ">
+                    <p className="text-[24px] font-medium ml-4 my-auto">El equipo</p>
+                  </div>
+                  <div className="flex flex-col justify-evenly">
+                    <div>
+                      <p className="text-[16px]">
+                      Buscamos conectar a las bodegas locales con el mundo entero, a través del uso de la tecnología blockchain. Para lograr este cometido hemos reunido el siguiente equipo de profesionales
+                      </p>
+                    </div>
+                    <div className='h-full'>
+
+                    <div className='flex flex-row'>
+                      <div className='bg-gray-200 m-5 flex-col'>
+                        <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        <h2>Federico Marino</h2>
+                        <p>Co Founder</p>
+                        <img src={linkedin} alt="linkedin" />
+                      </div>
+
+                      <div className='bg-gray-200 m-5 flex-col'>
+                        <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        <h2>Federico Marino</h2>
+                        <p>Co Founder</p>
+                        <img src={linkedin} alt="linkedin" />
+                      </div>
+
+                    </div>
+
+
+                    <div className='flex flex-row'>
+                      <div className='bg-gray-200 m-5 flex-col'>
+                        <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        <h2>Federico Marino</h2>
+                        <p>Co Founder</p>
+                        <img src={linkedin} alt="linkedin" />
+                      </div>
+
+                      <div className='bg-gray-200 m-5 flex-col'>
+                        <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        <h2>Federico Marino</h2>
+                        <p>Co Founder</p>
+                        <img src={linkedin} alt="linkedin" />
+                      </div>
+
+
+                    </div>
+                    <div className='flex flex-row'>
+                      <div className='bg-gray-200 m-5 flex-col'>
+                        <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        <h2>Federico Marino</h2>
+                        <p>Co Founder</p>
+                        <img src={linkedin} alt="linkedin" />
+                      </div>
+
+                      <div className='bg-gray-200 m-5 flex-col'>
+                        <img src={federico} alt="federico" className='rounded-xlinline-block rounded-full' />
+                        <h2>Federico Marino</h2>
+                        <p>Co Founder</p>
+                        <img src={linkedin} alt="linkedin" />
+                      </div>
+
+                    </div>
+
+
+
+                    </div>
+
+                  </div>
+          </SwiperSlide>
         </Swiper>
       </SwiperSlide>
     </Swiper>
